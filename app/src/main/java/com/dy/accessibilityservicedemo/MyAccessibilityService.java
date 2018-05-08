d22028579f655de93cd39e34070e55cdcfab8e7e
daiyao <991275257@qq.com>
2018-05-08 22:39:26 +0800

    增加滑动操作

--- app/src/main/java/com/dy/accessibilityservicedemo/MyAccessibilityService.java
+++ app/src/main/java/com/dy/accessibilityservicedemo/MyAccessibilityService.java
@@ -1,9 +1,12 @@
 package com.dy.accessibilityservicedemo;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
 import android.content.Intent;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.text.TextUtils;
@@ -12,7 +15,9 @@ import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.ACTION_CLICK;
+import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.ACTION_LONG_CLICK;
 import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.ACTION_SCROLL_BACKWARD;
+import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.ACTION_SCROLL_FORWARD;
 
 /**
  * @author DaiYao
@@ -40,9 +45,17 @@ public class MyAccessibilityService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         int eventType = event.getEventType();
-        Log.v(TAG, eventType + "");
-        AccessibilityNodeInfo nodeClientView = getRootInActiveWindow();
+//        switch (eventType) {
+//            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+//                //界面点击
+//                break;
+//            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+//                //界面文字改动
+//                break;
+//        }
+        Log.v(TAG, "eventType" + eventType);
 
+        AccessibilityNodeInfo nodeClientView = getRootInActiveWindow();
         findAllClientView(nodeClientView);
     }
 
@@ -73,21 +86,56 @@ public class MyAccessibilityService extends AccessibilityService {
                     wrap.getBoundsInScreen(bounds);
                     sBuffer.append("控件在父视图位置:")
                             .append(bounds);
-                } catch (Exception e) {
+                } catch (Exception ignored) {
 
                 }
                 Log.e(TAG, sBuffer.toString());
 
 
-                if ("相机".equals(text)) {
+                if ("日历".equals(text)) {
                     xjCount++;
-                    sBuffer.append("相机次数:").append(xjCount);
+                    sBuffer.append("日历次数:").append(xjCount);
                     if (xjCount > 5) {
                         AccessibilityNodeInfoCompat wrapParent = wrap.getParent();
-                        if (wrapParent.isScrollable()) {
-                            wrap.performAction(ACTION_SCROLL_BACKWARD);
+//                        wrap.performAction(ACTION_CLICK); //模拟点击
+
+                        //粒子滑动
+//                        Bundle arguments = new Bundle();
+//                        arguments.putInt(AccessibilityNodeInfo.ACTION_ARGUMENT_MOVEMENT_GRANULARITY_INT,
+//                                AccessibilityNodeInfo.MOVEMENT_GRANULARITY_PAGE);
+//                        arguments.putBoolean(AccessibilityNodeInfo.ACTION_ARGUMENT_EXTEND_SELECTION_BOOLEAN,
+//                                false);
+//
+//                        wrapParent.performAction(AccessibilityNodeInfo.ACTION_PREVIOUS_AT_MOVEMENT_GRANULARITY,
+//                                arguments);
+
+//                        performGlobalAction(AccessibilityService.GESTURE_SWIPE_LEFT);
+
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                            GestureDescription.Builder builder = new GestureDescription.Builder();
+
+                            Path path = new Path();
+                            path.moveTo(100, 500);
+                            path.lineTo(1000, 500);
+                            GestureDescription gesture = builder.addStroke(
+                                    new GestureDescription.StrokeDescription(
+                                            path,
+                                            10L,
+                                            200L))
+                                    .build();
+
+                            dispatchGesture(gesture, new GestureResultCallback() {
+                                @Override
+                                public void onCompleted(GestureDescription gestureDescription) {
+                                    super.onCompleted(gestureDescription);
+                                }
+
+                                @Override
+                                public void onCancelled(GestureDescription gestureDescription) {
+                                    super.onCancelled(gestureDescription);
+                                }
+                            }, null);
                         }
-//                            performGlobalAction(AccessibilityService.GESTURE_SWIPE_LEFT);
                     }
                 }
                 wrap.recycle();
