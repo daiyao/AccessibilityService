953cd6e5063ad020552a6989b6ca592076cf3643
daiyao <daiyao>
2018-05-08 17:17:13 +0800

    修改滚动

--- app/src/main/java/com/dy/accessibilityservicedemo/MyAccessibilityService.java
+++ app/src/main/java/com/dy/accessibilityservicedemo/MyAccessibilityService.java
@@ -1,18 +1,22 @@
 package com.dy.accessibilityservicedemo;
 
 import android.accessibilityservice.AccessibilityService;
+import android.annotation.SuppressLint;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.graphics.Rect;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.annotation.RequiresApi;
 import android.support.v4.view.accessibility.AccessibilityNodeInfoCompat;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
-
-import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.ACTION_CLICK;
-import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.ACTION_SCROLL_BACKWARD;
 
 /**
  * @author DaiYao
@@ -20,11 +24,16 @@ import static android.support.v4.view.accessibility.AccessibilityNodeInfoCompat.
 public class MyAccessibilityService extends AccessibilityService {
 
     private String TAG = "Accessibility";
+    private MyHandlerThread myHandlerThread;
+
+    private int xjCount = 0;
 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
         Log.v(TAG, "服务连接了,开始初始化工作...");
+        myHandlerThread = new MyHandlerThread();
+        myHandlerThread.start();
 
     }
 
@@ -41,16 +50,32 @@ public class MyAccessibilityService extends AccessibilityService {
     public void onAccessibilityEvent(AccessibilityEvent event) {
         int eventType = event.getEventType();
         Log.v(TAG, eventType + "");
-        AccessibilityNodeInfo nodeClientView = getRootInActiveWindow();
+        switch (eventType) {
+            //当通知栏发生改变时
+            case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
 
+                break;
+            //当窗口的状态发生改变时
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+                break;
+            default:
+                break;
+        }
+        AccessibilityNodeInfo nodeClientView = getRootInActiveWindow();
         findAllClientView(nodeClientView);
     }
 
-    private int xjCount = 0;
 
     private void findAllClientView(AccessibilityNodeInfo nodeClientView) {
         AccessibilityNodeInfoCompat wrap = AccessibilityNodeInfoCompat.wrap(nodeClientView);
-        if (wrap.getChildCount() == 0) {
+
+        int childCount;
+        try {
+            childCount = wrap.getChildCount();
+        } catch (Exception e) {
+            return;
+        }
+        if (childCount == 0) {
             String viewIdResourceName = wrap.getViewIdResourceName();
             CharSequence text = wrap.getText();
 
@@ -84,10 +109,12 @@ public class MyAccessibilityService extends AccessibilityService {
                     sBuffer.append("相机次数:").append(xjCount);
                     if (xjCount > 5) {
                         AccessibilityNodeInfoCompat wrapParent = wrap.getParent();
-                        if (wrapParent.isScrollable()) {
-                            wrap.performAction(ACTION_SCROLL_BACKWARD);
-                        }
-//                            performGlobalAction(AccessibilityService.GESTURE_SWIPE_LEFT);
+//                        if (wrapParent.isScrollable()) {
+//                            wrap.performAction(ACTION_SCROLL_BACKWARD);
+//                        }
+//                        performGlobalAction(AccessibilityService.GESTURE_SWIPE_LEFT);
+                        myHandlerThread.mHandler.sendEmptyMessage(0);
+//                        wrapParent.performAction(AccessibilityService.GESTURE_SWIPE_LEFT);
                     }
                 }
                 wrap.recycle();
@@ -98,6 +125,37 @@ public class MyAccessibilityService extends AccessibilityService {
                     findAllClientView(nodeClientView.getChild(i));
                 }
             }
+        }
+    }
+
+
+    class MyHandlerThread extends Thread {
+
+        public Handler mHandler;
+
+        @Override
+        public void run() {
+            Looper.prepare();
+            Looper looper = Looper.myLooper();
+            mHandler = new Handler(looper) {
+                @Override
+                public void handleMessage(Message msg) {
+                    super.handleMessage(msg);
+
+                    if (msg.what == 0) {
+
+                        long dowTime = SystemClock.uptimeMillis();
+
+                        Instrumentation   inst = new Instrumentation();
+                        inst.sendPointerSync(MotionEvent.obtain(dowTime, dowTime,
+                                MotionEvent.ACTION_MOVE, 0, 500, 0));
+                        inst.sendPointerSync(MotionEvent.obtain(dowTime, dowTime,
+                                MotionEvent.ACTION_MOVE, 1000, 500, 0));
+                    }
+
+                }
+            };
+            Looper.loop();
         }
     }
 
